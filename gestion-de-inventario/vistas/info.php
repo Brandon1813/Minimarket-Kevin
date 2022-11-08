28402b072ec7c3bbc3343faedf5b1792bdbcbbd4
Jhenci1803 <109631466+Jhenci1803@users.noreply.github.com>
2022-11-07 19:31:57 -0500

    add: Inventario
    Control de Inventario mas inicio de sesion

---
+++ gestion-de-inventario/vistas/info.php
@@ -0,0 +1,17 @@
+<div class="content is-medium">
+    <h1>Ayuda General</h1>
+    <p>Tranquilo Amigo Tendero aqui te aclaramos todas las dudas mas comunes del sistema </p>
+    <P>PARA QUE LA NAVEGACION SEA MUCHO MAS AMIGABLE EN TU TECLADO VAS A OPRIMIR LA TECLA <span class="button is-static">Cltrl</span>+<span class="button is-static">F</span> AL MISMO TIEMPO PARA QUE TE APAREZCA UN BUSCADOR Y PUEDAS BUSCAR LAS DUDAS MAS COMUNES COMO </P>
+    
+    <ul>
+      <li>1 Cambio de claves.</li>
+      <li>2 Eliminar Usuarios.</li>
+      <li>3 Creacion de usuarios Nuevos.</li>
+      <li>4 Regitro de productos y categorias.</li>
+      <li>5 Eliminar O ACTUALIZAR PRODUCTOS.</li>
+    </ul>
+  </div>
+    <br></br>
+    
+
+   
